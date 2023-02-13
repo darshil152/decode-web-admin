de207f9ff5b54887238b7ccd6ac3244e8a5d6333
Heaven Kapopara <heavenkapopara@gmail.com>
2023-02-13 17:33:52 +0530

    add chart

--- src/Chart.js
+++ src/Chart.js
@@ -1,72 +1,49 @@
-import React, { Component, useState, useSyncExternalStore, useEffect } from 'react'
+
+
+import React, { Component } from 'react'
 import ReactApexChart from 'react-apexcharts';
-import axios from 'axios';
-import { resolvePath } from 'react-router';
 
+export default class Chart extends Component {
+    constructor(props) {
+        super(props);
 
-export default function Chart() {
+        this.state = {
 
-
-    const title = [];
-    const price = [];
-
-    const [option, setObject] = useState({
-        chart: {
-            id: 'apexchart-example'
-        },
-        xaxis: {
-            category: []
-        }
-    })
-    const [series, setSeries] = useState([{
-        name: 'series-1',
-        data: [30, 40, 50, 44, 20, 91, 125, 87, 99]
-    }])
-
-
-    var config = {
-        method: 'get',
-        url: 'https://dummyjson.com/products',
-        headers: {}
-    };
-
-
-    useEffect(() => {
-        const id = [];
-        const price = [];
-        axios(config)
-            .then(function (response) {
-                console.log(response.data.products);
-                response.data.products.map(items => {
-                    console.log(items);
-                    id.push(items.id);
-                    price.push(items.price);
-                })
-
-
-                setObject({
-                    chart: {
-                        id: 'apexchart-example'
-                    },
-                    xaxis: {
-                        category: id
+            series: [144, 55],
+            options: {
+                chart: {
+                    width: 380,
+                    type: 'pie',
+                },
+                labels: ['Present', 'Absent',],
+                responsive: [{
+                    breakpoint: 480,
+                    options: {
+                        chart: {
+                            width: 200
+                        },
+                        legend: {
+                            position: 'bottom'
+                        }
                     }
-                })
-                setSeries([{
-                    name: 'series-1',
-                    data: price
-                }])
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }, [])
+                }]
+            },
 
 
-    return (
-        <div id="chart">
-            <ReactApexChart options={option} series={series} type="line" height={550} width={1300} />
-        </div>
+        };
+    }
 
-    )
+
+
+    render() {
+        return (
+
+
+            <div id="chart">
+                <ReactApexChart options={this.state.options} series={this.state.series} type="pie" width={380} />
+            </div>
+
+
+        );
+    }
 }
